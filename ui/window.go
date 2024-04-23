8aae1b5f711042abc3868d0551003237eb638d7d
Kiril Yakymchuk <stbestich@gmail.com>
2024-04-23 11:12:44 +0300

    Draw shape

--- ui/window.go
+++ ui/window.go
@@ -6,7 +6,7 @@ import (
 	"log"
 
 	"golang.org/x/exp/shiny/driver"
-	"golang.org/x/exp/shiny/imageutil"
+	//"golang.org/x/exp/shiny/imageutil"
 	"golang.org/x/exp/shiny/screen"
 	"golang.org/x/image/draw"
 	"golang.org/x/mobile/event/key"
@@ -135,10 +135,15 @@ func (pw *Visualizer) handleEvent(e any, t screen.Texture) {
 func (pw *Visualizer) drawDefaultUI() {
 	pw.w.Fill(pw.sz.Bounds(), color.RGBA{0, 128, 0, 255}, draw.Src) // Фон.
 
-	// TODO: Змінити колір фону та додати відображення фігури у вашому варіанті.
+	centerX := pw.sz.Bounds().Dx() / 2
+	centerY := pw.sz.Bounds().Dy() / 2
 
-	// Малювання білої рамки.
-	for _, br := range imageutil.Border(pw.sz.Bounds(), 10) {
-		pw.w.Fill(br, color.White, draw.Src)
-	}
+	rectWidth := 170
+	rectHeight := 400
+
+	verticalRect := image.Rect(centerX - rectHeight / 2, centerY - rectWidth / 2, centerX + rectHeight / 2, centerY + rectWidth / 2)
+	pw.w.Fill(verticalRect, color.RGBA{255, 255, 0, 255}, draw.Src)
+
+	horizontalRect := image.Rect(centerX - rectWidth / 2, centerY - rectHeight / 2, centerX + rectWidth / 2, centerY + rectHeight / 2)
+	pw.w.Fill(horizontalRect, color.RGBA{255, 255, 0, 255}, draw.Src)
 }
